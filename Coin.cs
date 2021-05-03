0674fabe3f5c731e8a5f9441c195d7124dc1babf
stkeven <33434288+stkeven@users.noreply.github.com>
2021-05-03 14:30:05 -0600

    Add files via upload

---
+++ Coin.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SCPart13
+{//Classes can help keep code organized when applications become large
+    public class Coin
+    {/// <summary>
+    /// declaration of variables
+    /// </summary>
+        private string sideUp;
+        //Allows randomization
+        Random rand;
+        //private variable declared
+        private string _sideup;
+        public Coin()
+        {//instantiates the class Random which is already available for use
+            rand = new Random();
+            //Declares a value for _sideup for default.
+            _sideup = "Heads";
+        }
+        /// <summary>
+        /// Logic to simulate a coinflip
+        /// </summary>
+        public void FlipCoin()
+        {//Gets 2 possible random integers starting at 0
+            int randomSide = rand.Next(2);
+            //if the flip is equal to 0...
+            if (randomSide == 0)
+            {//set sideup equal to heads
+                _sideup = "Heads";
+            }//if the flip isn't equal to 0
+            else
+            {//set sideup equal to tails
+                _sideup = "Tails";
+            }
+        }
+        /// <summary>
+        /// For returning the result of the computation
+        /// </summary>
+        /// <returns></returns>
+        public string GetSideUp()
+        {//Return the sideup result
+            return _sideup;
+        }
+       
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace SCPart13
+{
+    public partial class MainPage : ContentPage
+    {
+        //Calls the coin class and its constructor method to connect it to the mainpage
+        Coin coin = new Coin();
+        //Creates new coinResults Lists
+        List<string> coinResults = new List<string>();
+        public MainPage()
+        {   
+            InitializeComponent();
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BtnFlip_Clicked(object sender, EventArgs e)
+        {//flips the coin by telling the computer to run the logic in the coin class
+           coin.FlipCoin();
+            //Display the coin flip results to the user
+            DisplayAlert("Coin Results", coin.GetSideUp(), "Close");
+        }
+        /// <summary>
+        /// Flip the coin 5 times and display the result
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BtnFlip5x_Clicked(object sender, EventArgs e)
+        {//Creates new lists of the results wiping the old ones
+            LstViewCoin.ItemsSource = new List<string>();
+            //does the same thing but a different way using the coinResults
+            coinResults.Clear();
+
+            //Loop that runs flipcoin 5 times
+            for (int i = 0; i < 5; i++)
+            {//runs flipcoin
+                coin.FlipCoin();
+                //each time it runs flipcoin, it will add it to the result receiver
+                coinResults.Add(coin.GetSideUp());
+            }//sets the list view values equal to the coinResults
+            LstViewCoin.ItemsSource = coinResults;
+        }
+    }
+}
